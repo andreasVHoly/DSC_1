fc9ebd60fbf115e08943879307337c83a6de79ee
Andreas <andreas409@gmail.com>
2016-05-27 17:17:30 +0200

    some intellgience added to the way files are chosen and created on the resources

--- GridSim/examples/InputCreator.java
+++ GridSim/examples/InputCreator.java
@@ -7,15 +7,16 @@ import java.io.*;
 
 public class InputCreator {
 
- 
+    private LinkedList<LinkedList<Integer>> resourceAllocation;
 	private int noUsers, noResources, noFiles;
 	private String headRouter;
 	private Random rn;
 	private BufferedWriter bfr;
-	
-	
+
+
 	public InputCreator(int resources, int users){
 		rn = new Random();
+        resourceAllocation = new LinkedList<LinkedList<Integer>>();
 		try {
 			createNetwork();
 			createFiles(10,1024);
@@ -26,17 +27,18 @@ public class InputCreator {
 		catch (Exception e) {
 			e.printStackTrace();
 		}
-		 
+
 	}
-	
-//	#user_name router_name rc_name baud_rate list_of_grid_tasks
-//	#grid tasks can be: 
-//	#get filename
-//	#attribute filename
-//	#replicate filename resource
-//	#delete filename resource
-//user1 router2 Res_0 0.1 get testFile2 get testFile1 attribute testFile1 get fileF
-	
+
+    private boolean checkForFile(int resource, int file){
+        LinkedList<Integer> listoFiles = resourceAllocation.get(resource);
+        if (listoFiles.contains(file)){
+            return true;
+        }
+        return false;
+    }
+
+
 	//create no amount of users
 	public void createUsers(int no) throws IOException{
 		noUsers = no;
@@ -49,12 +51,12 @@ public class InputCreator {
 			router = rn.nextInt(5)+1;
 			blimit = rn.nextFloat()*10.0f;
 			res = rn.nextInt(noResources);
-			bfr.write("user" + Integer.toString(i) 
-			+ " router" + Integer.toString(router) 
+			bfr.write("user" + Integer.toString(i)
+			+ " router" + Integer.toString(router)
 			+ " Res_"  +Integer.toString(res)
 			+ " " + Float.toString(Math.round(blimit*10)/10.0f));
-			
-			
+
+
 			commands = rn.nextInt(5)+1;
 			for (int j = 0; j < commands; j++){
 				command =  rn.nextInt(4);
@@ -73,25 +75,33 @@ public class InputCreator {
 				case 2:
 					filamount = rn.nextInt(noFiles)+1;
 					resamount = rn.nextInt(noResources);
+                    while(!checkForFile(resamount,filamount)){
+                        filamount = rn.nextInt(noFiles)+1;
+    					resamount = rn.nextInt(noResources);
+                    }
 					bfr.write(" replicate file" + Integer.toString(filamount) + " Res_" + Integer.toString(resamount));
 					break;
 				//delete
 				case 3:
 					filamount = rn.nextInt(noFiles)+1;
 					resamount = rn.nextInt(noResources);
+                    while(!checkForFile(resamount,filamount)){
+                        filamount = rn.nextInt(noFiles)+1;
+    					resamount = rn.nextInt(noResources);
+                    }
 					bfr.write(" delete file" + Integer.toString(filamount) + " Res_" + Integer.toString(resamount));
 					break;
-					
+
 				}
 			}
 			bfr.write("\n");
-			
+
 		}
 		//close file
 		bfr.close();
 	}
-	
-	
+
+
 	//creates no amount of files
 	public void createFiles(int no, int sizeMax) throws IOException{
 		noFiles = no;
@@ -101,11 +111,11 @@ public class InputCreator {
 		for (int i = 1; i <= no; i++){
 			int size = rn.nextInt(sizeMax);
 			bfr.write("file" + Integer.toString(i) + " " + Integer.toString(size) + "\n");
-		}	
+		}
 		//close file
-		bfr.close();	
+		bfr.close();
 	}
-	
+
 	//creates the resources
 	public void createResources(int no, int sizeMin, int sizeMax) throws IOException{
 		noResources = no;
@@ -114,30 +124,42 @@ public class InputCreator {
 		int router, size, filamount, fileIndex;
 		float blimit;
 		//create no amount of resources
+
 		for (int i = 0; i < no; i++){
+            boolean[] fileUsed =  new boolean[noFiles+1];
+            for (int l = 0; l < noFiles+1; l++){
+                fileUsed[l] = false;
+            }
 			size = rn.nextInt(sizeMax);
 			blimit = rn.nextFloat()*10.0f;
 			router = rn.nextInt(5)+1;
-			filamount = rn.nextInt(noFiles)+1;	
-			bfr.write("Res_" + 
-					Integer.toString(i) + " " + 
-					Integer.toString(size+sizeMin) + " " + 
+			filamount = rn.nextInt(noFiles)+1;
+
+            LinkedList<Integer> temp = new LinkedList<Integer>();
+            resourceAllocation.add(i,temp);
+			bfr.write("Res_" +
+					Integer.toString(i) + " " +
+					Integer.toString(size+sizeMin) + " " +
 					Float.toString(Math.round(blimit*10)/10.0f) + " " +
 					"router" + Integer.toString(router) + " ");
-			
+
 			for (int j = 0; j < filamount; j++){
 				fileIndex = rn.nextInt(noFiles)+1;
-				
+                if (fileUsed[fileIndex]){
+                    continue;
+                }
+                fileUsed[fileIndex] = true;
+                resourceAllocation.get(i).add(fileIndex);
 				bfr.write("File" + Integer.toString(fileIndex) + " ");
 			}
-			bfr.write("\n");			
-		}	
+			bfr.write("\n");
+		}
 		//close file
 		bfr.close();
 	}
-	
-	
-	
+
+
+
 	//creates the paramaters
 	public void createParameters() throws IOException{
 		bfr = new BufferedWriter(new FileWriter("parameters.txt"));
@@ -151,21 +173,16 @@ public class InputCreator {
 		//close file
 		bfr.close();
 	}
-	
+
 	//creates the origial network
 	public void createNetwork() throws IOException{
 		bfr = new BufferedWriter(new FileWriter("network.txt"));
 		bfr.write("5\nrouter1\nrouter2\nrouter3\nrouter4\nrouter5\nrouter1 router2 0.1 10.0 1500\nrouter1 router5 0.1 10.0 1500\nrouter2 router5 0.1 10.0 1500\nrouter2 router3 0.1 10.0 1500\nrouter4 router5 0.1 10.0 1500");
 		bfr.close();
 	}
-	
+
 	public static void main(String args[]){
 		InputCreator ip = new InputCreator(10,20);
 	}
-	
+
 }
-
-
-
-
-
